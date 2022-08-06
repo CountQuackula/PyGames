0bc5476e15e49065bc6f05103c47e8adb5566136
CountQuackula <Faisal_umar_azhar@hotmail.com>
2022-08-06 14:25:16 +0930

    Added changeing window name for changing game etc nad cleaned up comments and code

--- mainScript.py
+++ mainScript.py
@@ -3,7 +3,6 @@ import sys, pygame, time
 from turtle import screensize;
 pygame.init();
 from GUIcomponentFuncs import *
-from mainMenu import *
 from menuCreator import *
 
 #creating a screen for program
@@ -12,24 +11,25 @@ screen = pygame.display.set_mode(screenSize);
 pygame.display.set_caption("Simple PyGames by Faisal");
 screen.fill((255,255,255));
 
+#initilizing an empty list to store user selections
+selection = [];
 
 #calling main menu to create it
 textMainMenu = ["Tower Of Hanoi", "Kakuro", "KenKen"];
-selection = menuCreator(screen, 3, textMainMenu);
+selection.append(menuCreator(screen, 3, textMainMenu));
 print(selection);
 
-if selection == 0:
+#
+if selection[0] == 0:
     #calling Tower of Hanoi menu
-    text = ["New Game", "Load Save", "Difficulty", "4"];
-    selection = menuCreator(screen, 4, text);
-    print(selection);
-elif selection == 1:
+    pygame.display.set_caption("Tower of Hanoi");
+elif selection[0] == 1:
     #calling Kakuro menu
-    text = ["1", "2", "3", "4"];
-    selection = menuCreator(screen, 4, text);
-    print(selection);
-elif selection == 2:
+    pygame.display.set_caption("Kakuro");
+elif selection[0] == 2:
     #calling KenKen menu
-    text = ["1", "2", "3", "4"];
-    selection = menuCreator(screen, 4, text);
-    print(selection);
+    pygame.display.set_caption("KenKen");
+
+text = ["New Game", "Load Save", "Difficulty", "Video Settings"];
+selection.append(menuCreator(screen, 4, text));
+print(selection);
